3cb05f50e3fcf40780e40bde7f06aaf50f8e6352
Leah Petersen <leahnpetersen@gmail.com>
2016-09-21 16:32:17 -0700

    started batch funtion, takes in url param indicating number of logs user wants to batch send all at once.

--- log_app.go
+++ log_app.go
@@ -11,6 +11,7 @@ import (
     // "bytes"
     "log"
     // "strings"
+    "time"
 )
 
 func check(e error) {
@@ -19,13 +20,13 @@ func check(e error) {
     }
 }
 
-
 func index(w http.ResponseWriter, r *http.Request) {
     t, _ := template.ParseFiles("templates/index.html")
     t.Execute(w, "templates/index.html")
 }
 
-func stack_traces(w http.ResponseWriter, r *http.Request, language string) {
+// currently only returns go stack trace
+func stack_traces(w http.ResponseWriter, r *http.Request) {
     // file contents as string
     trace, err := ioutil.ReadFile("models/go_stack_trace")
     check(err)
@@ -44,13 +45,12 @@ func stack_traces(w http.ResponseWriter, r *http.Request, language string) {
     // print stack traces to stdout & stderr
     fmt.Printf(str)
     fmt.Fprintln(os.Stderr, "hello world")
-    // use log
 
     http.Redirect(w, r, "/", http.StatusFound)
 }
 
+// prints log with random level to Stdout Stderr and reference-logging
 func levels(w http.ResponseWriter, r *http.Request) {
-
     // array of levels and level messages
     var levels_array = [6][2]string{ 
                 {"Fatal", "We're going doooowwwwnnnnn!!!!!!"}, 
@@ -61,23 +61,54 @@ func levels(w http.ResponseWriter, r *http.Request) {
                 {"Trace", "Happy hunting."},
             }
 
+    // picks random level and message
     message := levels_array[rand.Intn(len(levels_array))]
 
-
+    // opens reference-logging file
     f, err := os.OpenFile("var/log/reference-logging", os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
     check(err)
+
     defer f.Close()
 
-    log.SetOutput(io.MultiWriter(f, os.Stdout))
+    // sets output to print to file, stdout, stderr
+    log.SetOutput(io.MultiWriter(f, os.Stdout, os.Stderr))
 
     // print log with level and message
-    // log.Println("[" + message[0] + "] " + message[1])
+    log.Println("[" + message[0] + "] " + message[1])
+
     http.Redirect(w, r, "/", http.StatusFound)
+}
+
+// send batch logs all at once
+func batch(w http.ResponseWriter, r *http.Request) {
+    num := r.URL.Query().Get("num")
+
+    for i := 0; i < num; i++ {
+        fmt.Println("kwel")
+    }
+
+    fmt.Println(num)
+    http.Redirect(w, r, "/", http.StatusFound)
+}
+
+// call function every x millis
+func doEvery(d time.Duration, f func(time.Time)) {
+    for x := range time.Tick(d) {
+        f(x)
+    }
+}
+
+// reoccuring message
+func ahoy(t time.Time) {
+    log.Println("Ahoy.")
 }
 
 func main() {
     http.HandleFunc("/", index)
     http.HandleFunc("/stack_traces", stack_traces)
     http.HandleFunc("/levels", levels)
+    http.HandleFunc("/batch", batch)
+    // TODO this blocks the rest of the website
+    // doEvery(1000*time.Millisecond, ahoy)
     http.ListenAndServe(":8080", nil)
 }
